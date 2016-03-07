40f8c1616a9f74e7cac9d3f221914a7a85f86935
ItsMeVlad <vladimirsofroniev1@hotmail.com>
2016-03-07 21:21:29 +0000

    Schedule working but buggy

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,26 +28,71 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->call(function(){
-            $lastAggregatedTransactionId = DB::table('transaction_aggregation')->orderBy('id', 'desc')->take(1)->get();
+            $offset = 0;
+            $chunk = 100000;
+            $commissionSum = 0;
+            $commissionAverage = 0;
+            $saleSum = 0;
+            $saleAverage = 0;
+            $lastAggregatedTransactionId = DB::table('transaction_aggregation')->orderBy('last_transaction_id', 'desc')->take(1)->get();
             $sizeOfTransactionsTable = DB::table('transactions')->count();
             $sizeOfTransactionAggregationTable = DB::table('transaction_aggregation')->count();
             if(empty($lastAggregatedTransactionId)){
-                if($sizeOfTransactionsTable > 0){
-                    $offset = 0;
-                    $chunk = 100000;
-                    while($sizeOfTransactionAggregationTable < (0.8*$sizeOfTransactionsTable)){    
-                        $chunkFromTransactionsTable = DB::table('transactions')->orderBy('id', 'desc')->skip($offset)->take($chunk)->get();
-                        $sizeOfTransactionAggregationTable += $chunk;
-                        foreach($chunkFromTransactionsTable as $transaction){
-                            $transaction = json_decode(json_encode($transaction), true);
-                            DB::table('transaction_aggregation')->insert($transaction);
-                        } 
-                        $offset = $chunk;
-                        $chunk += $chunk;
+                DB::insert("INSERT INTO production.transaction_aggregation (id, last_transaction_id, commission_average, commission_sum, sale_average, sale_sum) VALUES(0,0,0,0,0,0) ;");
+                $lastAggregatedTransactionId = DB::table('transaction_aggregation')->orderBy('last_transaction_id', 'desc')->take(1)->get();
+                if($lastAggregatedTransactionId[0]->id == 0){
+                    if($sizeOfTransactionsTable > 0){
+                        while($sizeOfTransactionAggregationTable < (0.8*$sizeOfTransactionsTable)){    
+                            $chunkFromTransactionsTable = DB::table('transactions')->select('id')->orderBy('id', 'desc')->skip($offset)->take($chunk)->get();
+                            $chunkFromTransactionsTable = DB::table('transactions')->select('sale_amount')->orderBy('id', 'desc')->skip($offset)->take($chunk)->get();
+                            $chunkFromTransactionsTable = DB::table('transactions')->select('commission')->orderBy('id', 'desc')->skip($offset)->take($chunk)->get();
+                            foreach($chunkFromTransactionsTable as $transaction){
+                                if($commissionAverage == 0 && $saleAverage == 0){
+                                    $saleAverage = ($saleAverage + $transaction->sale_amount);
+                                    $commissionAverage = ($commissionAverage + $transaction->commission);
+                                }else{
+                                    $saleAverage = ($saleAverage + $transaction->sale_amount)/2;
+                                    $commissionAverage = ($commissionAverage + $transaction->commission)/2;
+                                }    
+                                $commissionSum += $transaction->commission;
+                                $saleSum += $transaction->sale_amount;
+                                $id = $transaction->id;
+                            }
+                            echo 'hello';
+                            $sizeOfTransactionAggregationTable += $chunk;
+                            $offset = $chunk;
+                            $chunk += $chunk;
+                            DB::table('transaction_aggregation')->where('id' , 0)->update(['id'=>0,'last_transaction_id'=> $id, 'commission_average' => $commissionAverage, 'commission_sum' => $commissionSum, 'sale_sum' => $saleSum, 'sale_average'=>$saleAverage]);
+                        }
                     }
-                }else{
-                    DB::insert("INSERT INTO production.transaction_aggregation (SELECT * FROM production.transactions);");
                 }
+            }else{
+                $transactionAggregationData = DB::table('transaction_aggregation')->select()->get();
+                $aggregated_commission_average = $transactionAggregationData[0]->commission_average;
+                $aggregated_commission_sum = $transactionAggregationData[0]->commission_sum;
+                $aggregated_sale_sum = $transactionAggregationData[0]->sale_sum;
+                $aggregated_sale_average = $transactionAggregationData[0]->sale_average;
+                $aggregated_transaction_id = $transactionAggregationData[0]->last_transaction_id;
+                $transactionData = DB::table('transactions')->select('id', 'sale_amount', 'commission')->where('id', '>', $aggregated_transaction_id)->take(10)->get();
+                foreach($transactionData as $transaction)
+                {
+                    if($commissionAverage == 0 && $saleAverage == 0){
+                        $saleAverage = ($saleAverage + $transaction->sale_amount);
+                        $commissionAverage = ($commissionAverage + $transaction->commission);
+                    }else{
+                        $saleAverage = ($saleAverage + $transaction->sale_amount)/2;
+                        $commissionAverage = ($commissionAverage + $transaction->commission)/2;
+                    }    
+                    $commissionSum += $transaction->commission;
+                    $saleSum += $transaction->sale_amount;
+                    $id = $transaction->id;
+                }
+                $aggregated_commission_sum += $commissionSum;
+                $aggregated_commission_average += $commissionAverage;
+                $aggregated_sale_average += $saleAverage;
+                $aggregated_sale_sum += $saleSum;
+                DB::table('transaction_aggregation')->where('id' , 0)->update(['id'=>0,'last_transaction_id'=> $id, 'commission_average' => $commissionAverage, 'commission_sum' => $commissionSum, 'sale_sum' => $saleSum, 'sale_average'=>$saleAverage]);
+                // DB::insert("INSERT INTO production.transaction_aggregation (SELECT * FROM production.transactions);");
             }
             //$nonAggregatedData = DB::table('transactions')->select('id','sale_amount','commission')->where('id','>',$lastAggregatedTransactionId[0]->id)->get();
             
