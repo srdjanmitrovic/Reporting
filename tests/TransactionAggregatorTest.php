d5743fcbc516415a265fdc80792f8eba19859e17
ItsMeVlad <vladimirsofroniev1@hotmail.com>
2016-03-16 21:58:41 +0000

    TransactionRepository test compelte. Started TransactionAggregator test.

---
+++ tests/TransactionAggregatorTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Aggregation\TransactionAggregator;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TransactionAggregatorTest extends TestCase
+{
+	
+	use DatabaseTransactions;
+
+	public function setUp()
+	{
+		
+	}
+
+	public function testIf()
+	{
+		# code...
+	}
+}
--- tests/TransactionRepositoryTest.php
+++ tests/TransactionRepositoryTest.php
@@ -1,30 +1,86 @@
 <?php 
 
-use DB;
 use App\Reporting\TransactionRepository;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class TransactionRepositoryTest extends TestCase
 {
-
+	/**
+	 * To roll back database after inserting data.
+	 */
 	use DatabaseTransactions;
 
+	/**
+	 * Date for transaction set up.
+	 * 
+	 * @var array $date
+	 */
 	private $date = array('month'=>03, 'day'=>05);
+
+	/**
+	 * To store data from the aggregation table.
+	 * 
+	 * @var array stdClass
+	 */
+	private $results;
 	
+
+	/**
+	 * Set up function for test.
+	 * 
+	 * @return void
+	 */
 	public function setUp()
 	{
 		$this->repository = new TransactionRepository($this->date);
 		parent::setUp();
+		DB::table('transaction_aggregation')->insert(['day'=>03, 'month'=>05, 'last_transaction_id'=>100, 'commission_average'=>5, 'commission_sum'=>100, 'sale_average'=>35, 'sale_sum'=>197,'transaction_count'=>35]);
+		$results = DB::table('transaction_aggregation')->select()->get();
+		$this->results = reset($results);
 	}
 
+	/**
+	 * Test that date is set.
+	 * 
+	 * @return void
+	 */
 	public function testThatDateCanBeSet()
 	{	
 		$this->assertEquals($this->date, $this->repository->date);
 	}
 
-	public function testGettingAggregatedValues()
+	/**
+	 * Test that passed aggregated values are correct.
+	 * 
+	 * @return void
+	 */
+	public function testLastTransaction()
 	{
-		DB::table('transaction_aggregation')->insert(['day'=>03, 'month'=>05, 'last_transaction_id'=>100, 'commission_average'=>5, 'commission_sum'=>100, 'sale_average'=>35, 'sale_sum'=>197,'transaction_count'=>35]);
-		// $this->assertEquals(array('month'=>03, 'day'=>5), $this->repository->date);
+		$this->assertEquals(100, $this->results->last_transaction_id);
+	}
+
+	public function testCommissionAverage()
+	{
+		$this->assertEquals(5, $this->results->commission_average);
+	}
+
+	public function testCommissionSum()
+	{
+		$this->assertEquals(100, $this->results->commission_sum);
+	}
+
+	public function testSaleAverage()
+	{
+		$this->assertEquals(35, $this->results->sale_average);
+	}
+
+	public function testSaleSum()
+	{
+		$this->assertEquals(197, $this->results->sale_sum);
+	}
+
+	public function testTransactionCount()
+	{
+		$this->assertEquals(35, $this->results->transaction_count);
 	}
 }
